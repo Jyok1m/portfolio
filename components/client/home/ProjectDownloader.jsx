77d3973698583dd4226024366fa662e8625b9779
Joachim Jasmin <118119984+Jyok1m@users.noreply.github.com>
2023-10-28 07:38:41 +0200

    Update ProjectDownloader.jsx

--- components/client/home/ProjectDownloader.jsx
+++ components/client/home/ProjectDownloader.jsx
@@ -11,7 +11,7 @@ export default function ProjectDownloader() {
 
 	return (
 		<div className="flex mt-5 h-8 hover:cursor-pointer" style={{ alignItems: "center" }} onClick={() => download(fileUrl, filename)}>
-			<p className="text-sm leading-6 text-zinc-100 hover:underline">Télécharger le ZIP</p>
+			<p className="text-sm leading-6 text-zinc-800 dark:text-zinc-100 hover:underline">Télécharger le ZIP</p>
 			<ArrowDownIcon className="h-5 w-5 ml-2 text-zinc-100" />
 		</div>
 	);
